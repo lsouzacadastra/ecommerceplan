5fba514d772577b317ce2fec00af2afc43f1053b
Jeferson <jefferson.aurelio@cadastra.com>
2022-04-28 16:40:21 -0300

    Ajuste para TrackerService

--- app/Services/TrackerService.php
+++ app/Services/TrackerService.php
@@ -4,20 +4,86 @@ namespace App\Services;
 
 use App\Models\Visita;
 use App\Models\VisitaMongo;
+use App\Models\VisitaTeste;
 use App\Solutions\Util\Util;
+use Illuminate\Support\Facades\DB;
 
 class TrackerService
 {
-    public static function getDados($data_de = false, $data_ate = false)
-    {
-        $dados = VisitaMongo::select('chave_sessao', 'ip', 'data', 'url', 'dispositivo', 'referrer');
+    public function registraVisista($dados){
 
-        if ($data_de && $data_ate) {
-            $dados->whereBetween('data', [Util::formatDataParaMongo($data_de), Util::formatDataParaMongo($data_ate, true)] );
+        //Verifica se visita existe
+        $visita_exist = VisitaTeste::where('chave_sessao', $dados['k'])->get();
+
+        //Cria visita
+        if(empty(@$visita_exist[0]['chave_sessao'])){
+
+            $ip = $dados['ip'];
+            $dados['localizacao'] = (array) json_decode( file_get_contents("http://ip-api.com/json/$ip") );
+            
+            $origem = '';
+        
+            if (strpos($dados['referrer'], 'google') !== false) {
+                
+                //Anúncio google
+                if (strpos($dados['url'], 'gclid') !== false) {
+                    $origem = 'anuncio_google';
+                //Busca google
+                }else{
+                    $origem = 'google';
+                }
+            
+            //Direto
+            }else{
+                $origem = 'direto';
+            }
+        
+            $visita = [
+                'ip' => $ip, 
+                'data' => date('Y-m-d H:i:s'), 
+                'chave_sessao' => $dados['k'],
+                'dispositivo' => $dados['dispositivo'],
+                'pais' => $dados['localizacao']['country'], 
+                'paisc' => $dados['localizacao']['countryCode'], 
+                'estado' => $dados['localizacao']['regionName'], 
+                'estadoc' => $dados['localizacao']['region'],
+                'cidade' => $dados['localizacao']['city'],
+                'origem' => $origem,
+                'referrer' => $dados['referrer'],
+                'resolucao' => $dados['resolucao'],
+                'os' => $dados['os'],
+                "paginas" => [
+                    [
+                    'hora' => date('Y-m-d H:i:s'), 
+                    'url' => $dados['url']
+                    ]
+                ]
+            ];
+
+            VisitaTeste::create($visita);
+
+        //Atualiza URLs
+        }else{
+
+            $paginas = $visita_exist[0]['paginas'];
+            $atualizar = true;
+            
+            foreach($paginas as $v){
+                if($v['url'] == $dados['url']){
+                    $atualizar = false;
+                    break;
+                }
+            }
+
+            if($atualizar){
+                $paginas_atualizar = $visita_exist[0]['paginas'];
+                $paginas_atualizar[] = ['hora' => date('Y-m-d H:i:s'), 'url' => $dados['url'] ];
+
+                VisitaTeste::where( array ('chave_sessao' => $dados['k']) )->update([ 'paginas' => $paginas_atualizar], ['upsert' => true] );
+            }
         }
 
-        $dados->groupBy('chave_sessao');
 
-        return $dados;
+        return json_encode(["sucess" => true]);
     }
 }
